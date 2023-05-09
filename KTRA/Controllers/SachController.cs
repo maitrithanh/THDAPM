7902e2d669b7e80ba90237b02a1435dc839ec17b
Mai Tri Thanh <56778099+maitrithanh@users.noreply.github.com>
2023-05-10 00:31:38 +0700

    first commit

---
+++ KTRA/Controllers/SachController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KTRA.Models;
+
+namespace KTRA.Controllers
+{
+    public class SachController : Controller
+    {
+        THDAPMEntities db = new THDAPMEntities();
+        
+        // GET: Sach
+        public ActionResult Index()
+        {
+            var dsSach = db.Saches.OrderByDescending(s => s.TenSach).ToList();
+            return View(dsSach);
+        }
+        
+        public ActionResult SachTheoTacGia(int id)
+        {
+            return View("Index", db.Saches.Where(tg => tg.MaTacGia == id).ToList());
+        }
+        // GET: Sach/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(db.Saches.Where(s => s.MaSach == id).FirstOrDefault());
+        }
+
+        // GET: Sach/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaTacGia = new SelectList(db.TacGias, "MaTacGia", "TenTacGia");
+            return View();
+        }
+
+        // POST: Sach/Create
+        [HttpPost]
+        public ActionResult Create(Sach sach)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(sach.TenSach))
+                    ModelState.AddModelError(string.Empty, "Tên sach không được để trống");
+                if (ModelState.IsValid)
+                {
+                    db.Saches.Add(sach);
+                    db.SaveChanges();
+                    return Redirect("/Sach/Index");
+                }
+            }
+            ViewBag.MaTacGia = new SelectList(db.TacGias, "MaTacGia", "TenTacGia", sach.MaTacGia);
+            return View();
+        }
+
+        // GET: Sach/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaTacGia = new SelectList(db.TacGias, "MaTacGia", "TenTacGia", sach.MaTacGia);
+            return View(sach);
+        }
+
+        // POST: Sach/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Sach sach)
+        {
+            if (ModelState.IsValid)
+            {
+                var sanPhamDB = db.Saches.FirstOrDefault(kh => kh.MaSach == sach.MaSach);
+                if (sanPhamDB != null)
+                {
+                    sanPhamDB.TenSach = sach.TenSach;
+                    sanPhamDB.DonGia = sach.DonGia;
+                    sanPhamDB.MaTacGia = sach.MaTacGia;
+                }
+                db.SaveChanges();
+                return Redirect("/Sach/Index");
+            }
+            ViewBag.MaTacGia = new SelectList(db.TacGias, "MaTacGia", "TenTacGia", sach.MaTacGia);
+            return View(sach);
+        }
+
+        // GET: Sach/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // POST: Sach/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sach sach = db.Saches.Find(id);
+            db.Saches.Remove(sach);
+            db.SaveChanges();
+            return Redirect("/Sach/Index");
+        }
+    }
+}
---
+++ KTRA/Controllers/TacGiaController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KTRA.Models;
+
+namespace KTRA.Controllers
+{
+    public class TacGiaController : Controller
+    {
+
+        THDAPMEntities db = new THDAPMEntities();
+
+        // GET: Sach
+        public ActionResult Index()
+        {
+            var dsTacGia = db.TacGias.OrderByDescending(s => s.TenTacGia).ToList();
+            return View(dsTacGia);
+        }
+
+        public ActionResult Shop()
+        {
+            var dsSach = db.Saches.OrderByDescending(s => s.TenSach).ToList();
+            return View(dsSach);
+        }
+
+        public ActionResult PartialTacGia()
+        {
+            return PartialView(db.TacGias.OrderByDescending(tg => tg.TenTacGia).ToList());
+        }
+
+        // GET: Sach/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(db.TacGias.Where(s => s.MaTacGia == id).FirstOrDefault());
+        }
+
+        // GET: Sach/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sach/Create
+        [HttpPost]
+        public ActionResult Create(TacGia tg)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(tg.TenTacGia))
+                    ModelState.AddModelError(string.Empty, "Tên sach không được để trống");
+                if (ModelState.IsValid)
+                {
+                    db.TacGias.Add(tg);
+                    db.SaveChanges();
+                    return Redirect("/TacGia/Index");
+                }
+            }
+            return View();
+        }
+
+        // GET: Sach/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var sach = db.TacGias.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // POST: Sach/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, TacGia tg)
+        {
+            if (ModelState.IsValid)
+            {
+                var sanPhamDB = db.TacGias.FirstOrDefault(kh => kh.MaTacGia == tg.MaTacGia);
+                if (sanPhamDB != null)
+                {
+                    sanPhamDB.TenTacGia = tg.TenTacGia;
+                }
+                db.SaveChanges();
+                return Redirect("/TacGia/Index");
+            }
+            return View(tg);
+        }
+
+        // GET: Sach/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var sach = db.TacGias.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // POST: Sach/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TacGia tg = db.TacGias.Find(id);
+            db.TacGias.Remove(tg);
+            db.SaveChanges();
+            return Redirect("/Sach/Index");
+        }
+    }
+}
